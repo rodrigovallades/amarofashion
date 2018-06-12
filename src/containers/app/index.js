64a7b5a749c4ee732d8a530ce8cf3b708cc15f53
rodrigovallades <rodrigo.vallades@gmail.com>
2018-06-12 15:04:44 -0300

    Creating store and initial pages

---
+++ src/containers/app/index.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch,
+  Redirect
+} from "react-router-dom";
+import Products from '../products';
+
+const App = () => (
+  <Router>
+    <div>
+      <Switch>
+        <Route path="/products" exact component={Products} />
+        <Redirect from="/" to="/products" />
+      </Switch>
+    </div>
+  </Router>
+);
+
+export default App;
---
+++ src/containers/products/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export default () => (
+  <div>
+    <h1>Products</h1>
+  </div>
+);
